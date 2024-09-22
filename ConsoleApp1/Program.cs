09315f933c10ac8a0cc7f3593779269b3f5925cb
Mikhail Ivashkin <mihail96-96@mail.ru>
2024-09-22 19:31:54 +0500

    Simple Interface Realization

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using MyProject;
+using System.Text.Json;
+
+namespace MyProject
+{
+    public interface MyFirstInterFace
+    {
+        List<string> Names { set; get; }
+        void AddName(string name);
+        
+    }
+    public class MyClass : MyFirstInterFace
+    {
+        public List<string> Names { set; get; } = new List<string> {"Misha"};
+        void MyFirstInterFace.AddName(string name)
+        {
+            Names.Add(name);
+        }
+        
+        }
+
+    }
+    public class MyProgram
+    {
+        public static void Main(string[] args)
+        {
+            MyFirstInterFace MyClass = new MyClass();
+            MyClass.AddName("Masha");
+        foreach (string name in MyClass.Names)
+        {
+            Console.WriteLine(name);
+        }
+    }       
+}
+
